a0096cd7f5014e0faefedd63a97021af13de9d76
Karthik-kommineni <karthik.kommineni@copart.com>
2025-11-15 18:41:25 -0600

    commit v1.0.7

--- app/templates_viewer.py
+++ app/templates_viewer.py
@@ -1,18 +1,21 @@
 import streamlit as st
+import importlib
 from templates.binary_search_templates import render_binary_search_template
 from templates.dfs_template import render_dfs_template
 from templates.bfs_template import render_bfs_template
 from templates.queue_template import render_queue_template
 from templates.sorting_template import render_sorting_template
-from templates.two_pointer_template import render_two_pointer_template
-from templates.sliding_window_template import render_sliding_window_template
+
+# Import modules with numeric prefixes using importlib
+two_pointer_module = importlib.import_module('templates.011_two_pointer_template')
+sliding_window_module = importlib.import_module('templates.012_sliding_window_template')
 
 from templates.stack_template import render_stack_template
 from templates.heap_template import render_heap_template
 
 TEMPLATE_RENDERERS = {
-    "Two Pointer": render_two_pointer_template,
-    "Sliding Window": render_sliding_window_template,
+    "Two Pointer": two_pointer_module.render_two_pointer_template,
+    "Sliding Window": sliding_window_module.render_sliding_window_template,
     "Stack": render_stack_template,
     "Heap": render_heap_template,
     "Sorting": render_sorting_template,
